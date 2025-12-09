5ba13e2a36338c7ddfc3a987ec8cb72df345a564
Factorito <redbulue0509@gmail.com>
2025-12-09 14:20:15 +0900

    과제 제출

--- logger.h
+++ logger.h
@@ -1,9 +1,11 @@
-
+#pragma once
 #include <iostream>
 #include <fstream>
-#include <mutex>
 #include <thread>
 #include <string>
+#include <memory>
+#include <mutex>
+
 
 class Logger {
 private:
@@ -11,15 +13,35 @@ private:
     static std::mutex init_mtx;
     static std::mutex write_mtx;
     std::ofstream logFile;
-
-
-public:
-    static Logger* getInstance(const std::string& filename = "Test/output2.txt") {
-        return nullptr;
+    Logger(const std::string& filename) {
+        logFile.open(filename, std::ios::trunc);
+        if (!logFile.is_open()) {
+            throw std::runtime_error("Failed to open log file.");
+        }else{
+            log("[Init] Logger started.");
+        }
     }
 
+public:
+    ~Logger() {
+        if (logFile.is_open()) {
+            log("[Shutdown] Logger closed.");
+            logFile.close();
+        }
+    }
+
+    static Logger* getInstance(const std::string& filename = "Test/output2.txt") {
+        if(instance == nullptr){
+            std::lock_guard<std::mutex> Lock(init_mtx);
+            instance.reset(new Logger(filename));
+        }
+        return instance.get();
+    }
+    
+
     void log(const std::string& message) {
-        /* TODO */
+        std::lock_guard<std::mutex> Lock(write_mtx);
+        logFile << message << std::endl;
     }
 
 };
@@ -27,4 +49,3 @@ public:
 std::unique_ptr<Logger> Logger::instance;
 std::mutex Logger::init_mtx;
 std::mutex Logger::write_mtx;
-
